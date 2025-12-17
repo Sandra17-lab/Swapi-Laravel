25a0422f98d0202464d9ec9df04eb7b9978c35fc
Sandra <Sandra17lunaz@gmail.com>
2025-12-16 20:22:11 -0600

    otro commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
-    $middleware->trustProxies(at: \App\Http\Middleware\TrustProxies::class);
+    ->withMiddleware(function (Middleware $middleware) {
+    //
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
